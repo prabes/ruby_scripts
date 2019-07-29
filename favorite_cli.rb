87809bf52cbbc2d1db782d5e62b259a6c9ec97a8
rdeyvil <rdeyvil11@gmail.com>
2019-07-29 13:59:23 +0545

    Insert data into table

--- favorite_cli.rb
+++ favorite_cli.rb
@@ -4,30 +4,36 @@ begin
   con =PG.connect :dbname => 'favdb', :user => 'rdeyvil'
   puts con.server_version
 
-  con.exec "DROP TABLE IF EXISTS favlist"
-  con.exec "CREATE TABLE favlist(id INTEGER PRIMARY KEY, username VARCHAR(20), fav_food VARCHAR(20), fav_place VARCHAR(20) )"
+  #con.exec "DROP TABLE IF NOT EXISTS favlist"
+  con.exec "CREATE TABLE IF NOT EXISTS favlist(id SERIAL PRIMARY KEY, username VARCHAR(20), fav_food VARCHAR(20), fav_place VARCHAR(20) )"
 
-  con.exec "INSERT INTO favlist "
   details = con.exec "SELECT * FROM favlist"
+  
   details.each do |detail|
-    puts "|%s| |%s| |%s|" [detail[id], detail[username], detail[fav_food], detail[fav_place]]
+    puts "|%s| |%s| |%s| |%s|" [ detail[id], detail[username], detail[fav_food], detail[fav_place]]
   end
+  
+  puts"Enter UserName:"
+  username = gets.chomp.to_str
+
+  puts"Enter Favorite Food:"
+  fav_food = gets.chomp.to_str
+
+  puts"Enter Favorite Place:"
+  fav_place = gets.chomp.to_str
+  
+  con.exec "INSERT INTO favlist (username, fav_food, fav_place) VALUES ('#{username}','#{fav_food}','#{fav_place}')"
 rescue PG::Error => e 
   puts e.message
 
 ensure
 
-  con.close if con 
+  details.clear if details
+  con.close if con
+   
 end
 
-puts"Enter UserName:"
-username = gets.chomp.to_str
 
-puts"Enter Favorite Food:"
-fav_food = gets.chomp.to_str
-
-puts"Enter Favorite Place:"
-fav_place = gets.chomp.to_str
 
 
 puts"Hello #{username}! \nVisit #{fav_place} and I'll give you #{fav_food}."
